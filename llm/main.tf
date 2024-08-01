6712bf3d7d5390e821956175e35a2629b56dbc07
Scott Cronin <j.scott.cronin@gmail.com>
2024-08-02 02:34:35 +0700

    adding bedrock permissions for the container

--- llm/main.tf
+++ llm/main.tf
@@ -13,6 +13,10 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_ssm_parameter" "latest_image_tag" {
+  name = "/app/fastapi-llm/latest-image-tag"
+}
+
 module "vpc" {
   source   = "./modules/vpc"
   name     = var.project_name
@@ -25,7 +29,7 @@ module "ecs" {
   name                 = var.project_name
   vpc_id               = module.vpc.vpc_id
   private_subnet_ids   = module.vpc.private_subnet_ids
-  container_image      = var.container_image
+  container_image      = "${var.container_image}:${data.aws_ssm_parameter.latest_image_tag.value}"
   container_port       = var.container_port
   alb_target_group_arn = module.alb.target_group_arn
 }
--- llm/modules/ecs/main.tf
+++ llm/modules/ecs/main.tf
@@ -9,6 +9,7 @@ resource "aws_ecs_task_definition" "this" {
   cpu                      = 256
   memory                   = 512
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
 
   container_definitions = jsonencode([
     {
@@ -84,4 +85,44 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${var.name}-ecsTaskRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+resource "aws_iam_policy" "bedrock_access" {
+  name        = "${var.name}-bedrock-access"
+  path        = "/"
+  description = "IAM policy for accessing AWS Bedrock"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "bedrock:InvokeModel",
+          "bedrock:ListFoundationModels"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_role_bedrock" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.bedrock_access.arn
 }
